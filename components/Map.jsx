aa0c11a4fda5730ea5ee97b336414b098c6578c5
Kyle Schiller <kyleschiller@MacBook-Pro-2.local>
2020-09-23 04:07:49 -0700

    add pointer cursor on clickable states

--- components/Map.jsx
+++ components/Map.jsx
@@ -59,23 +59,30 @@ const MapChart = ({ filterOptions, setFilterOptions }) => {
           {({ geographies }) => (
             <>
               {geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  stroke="#FFF"
-                  geography={geo}
-                  fill="#6f44ff"
-                  opacity={statesSet.has(geo.properties.name) ? 1 : 0.15}
-                  onClick={() => {
-                    if (statesSet.has(geo.properties.name)) {
-                      handleClick(geo.properties.name)
-                    }
-                  }}
-                  onMouseOver={() => {
-                    if (statesSet.has(geo.properties.name)) {
-                      handleHover(geo.properties.name)
-                    }
-                  }}
-                />
+                <g cursor={statesSet.has(geo.properties.name) ? 'pointer' : ''}>
+                  <Geography
+                    style={{
+                      cursor: 'pointer',
+                      fill: 'red',
+                      border: '1px solid black',
+                    }}
+                    key={geo.rsmKey}
+                    stroke="#FFF"
+                    geography={geo}
+                    fill="#6f44ff"
+                    opacity={statesSet.has(geo.properties.name) ? 1 : 0.15}
+                    onClick={() => {
+                      if (statesSet.has(geo.properties.name)) {
+                        handleClick(geo.properties.name)
+                      }
+                    }}
+                    onMouseOver={() => {
+                      if (statesSet.has(geo.properties.name)) {
+                        handleHover(geo.properties.name)
+                      }
+                    }}
+                  />
+                </g>
               ))}
             </>
           )}
