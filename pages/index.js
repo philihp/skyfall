49489b22b22b688f21866c7ac828c7684db08368
Kyle Schiller <kyleschiller@MacBook-Pro-2.local>
2020-09-23 16:23:01 -0700

    lint

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,12 @@ import Dropdown from '../components/Dropdown'
 import Header from '../components/Header'
 import Scatterplot from '../components/Scatterplot'
 import Map from '../components/Map'
-import { FILTER_FIELDS, BREAK_CHAR } from '../setup/config'
-import { readData, readColumns } from '../lib/readData'
+import {
+  namedAndStyledColumns,
+  FILTER_FIELDS,
+  BREAK_CHAR,
+} from '../setup/config'
+import readData from '../lib/readData'
 import { stringNumToFloat } from '../utils/utils'
 
 const styleSheet = () => ({
@@ -26,7 +30,7 @@ const styleSheet = () => ({
   },
 })
 
-function Index({ data, columns }) {
+function Index({ data }) {
   const [styles, cx] = useStyles(styleSheet)
   const [filterOptions, setFilterOptions] = useState({})
   const [hoveredIdx, setHoveredIdx] = useState(-1)
@@ -144,6 +148,7 @@ function Index({ data, columns }) {
     })
     .map((idx) => data[idx])
 
+  console.log(namedAndStyledColumns)
   return (
     <div className={styles.container}>
       <Header />
@@ -180,7 +185,7 @@ function Index({ data, columns }) {
         <DataTable
           noHeader
           highlightOnHover
-          columns={columns}
+          columns={namedAndStyledColumns}
           data={filteredData}
         />
       </Spacing>
@@ -190,12 +195,10 @@ function Index({ data, columns }) {
 
 export const getStaticProps = async () => {
   const data = await readData()
-  const columns = await readColumns()
 
   return {
     props: {
       data,
-      columns,
     },
   }
 }
--- setup/config.js
+++ setup/config.js
@@ -1,3 +1,5 @@
+import { dollarToFloat } from '../utils/utils'
+
 export const FILTER_FIELDS = {
   candidate_gender: {},
   state: {},
@@ -33,14 +35,14 @@ export const ACTIVE_COLUMNS = new Set([
 export const CUSTOM_RENDERERS = {
   amt_raised_diff_2020: [
     {
-      when: (row) => row.amt_raised_diff_2020 < 0,
+      when: (row) => dollarToFloat(row.amt_raised_diff_2020) < 0,
       style: {
         backgroundColor: 'rgba(242, 38, 19, 0.9)',
         color: 'white',
       },
     },
     {
-      when: (row) => row.amt_raised_diff_2020 > 0,
+      when: (row) => dollarToFloat(row.amt_raised_diff_2020) > 0,
       style: {
         backgroundColor: 'rgba(19, 38, 242, 0.9)',
         color: 'white',
@@ -48,3 +50,114 @@ export const CUSTOM_RENDERERS = {
     },
   ],
 }
+
+const rawColumns = [
+  { selector: 'candidate_name', sortable: true },
+  { selector: 'candidate_gender', sortable: true },
+  { selector: 'candidate_race', sortable: true },
+  { selector: 'state', sortable: true },
+  {
+    selector: 'is_gerrymandering',
+    sortable: true,
+  },
+  {
+    selector: 'is_competitive_senate',
+    sortable: true,
+  },
+  {
+    selector: 'is_presidential_swing',
+    sortable: true,
+  },
+  { selector: 'office_type', sortable: true },
+  { selector: 'district', sortable: true },
+  {
+    selector: 'clinton_vote_share_2016',
+    sortable: true,
+  },
+  {
+    selector: 'dem_state_leg_vote_share_2018',
+    sortable: true,
+  },
+  { selector: 'dem_votes_2018', sortable: true },
+  { selector: 'rep_votes_2018', sortable: true },
+  { selector: 'vote_diff_2018', sortable: true },
+  {
+    selector: 'vote_diff_pct_2018',
+    sortable: true,
+  },
+  {
+    selector: 'dem_amt_raised_2018',
+    sortable: true,
+  },
+  {
+    selector: 'rep_amt_raised_2018',
+    sortable: true,
+  },
+  {
+    selector: 'amt_raised_diff_2018',
+    sortable: true,
+  },
+  {
+    selector: 'amt_raised_diff_pct_2018',
+    sortable: true,
+  },
+  {
+    selector: 'party_in_seat_pre2020',
+    sortable: true,
+  },
+  {
+    selector: 'incumbent_running_2020',
+    sortable: true,
+  },
+  {
+    selector: 'dem_amt_raised_2020',
+    sortable: true,
+  },
+  {
+    selector: 'rep_amt_raised_2020',
+    sortable: true,
+  },
+  {
+    selector: 'amt_raised_diff_2020',
+    sortable: true,
+    conditionalCellStyles: [
+      {
+        when: (row) => row.amt_raised_diff_2020 < 0,
+        style: {
+          backgroundColor: 'rgba(242, 38, 19, 0.9)',
+          color: 'white',
+        },
+      },
+      {
+        when: (row) => row.amt_raised_diff_2020 > 0,
+        style: {
+          backgroundColor: 'rgba(19, 38, 242, 0.9)',
+          color: 'white',
+        },
+      },
+    ],
+  },
+  {
+    selector: 'projected_dollars_needed_to_win',
+    sortable: true,
+  },
+  { selector: 'donations_needed', sortable: true },
+]
+
+const activeColumns = rawColumns.filter((col) =>
+  ACTIVE_COLUMNS.has(col.selector)
+)
+
+export const namedAndStyledColumns = activeColumns.map((col) => ({
+  ...col,
+  conditionalCellStyles:
+    CUSTOM_RENDERERS[col.selector] === undefined
+      ? []
+      : CUSTOM_RENDERERS[col.selector],
+  name:
+    CUSTOM_COLUMN_NAMES[col.selector] !== undefined
+      ? CUSTOM_COLUMN_NAMES[col.selector]
+      : `${col.selector[0].toUpperCase()}${col.selector
+          .replaceAll('_', ' ')
+          .slice(1)}`,
+}))
