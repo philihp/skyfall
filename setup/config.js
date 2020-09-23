110fcf7f54fb05d97ff30de76f6aebae1d477001
Kyle Schiller <kyleschiller@gmail.com>
2020-09-23 02:47:01 -0700

    lint (#27)
    Co-authored-by: Kyle Schiller <kyleschiller@MacBook-Pro-2.local>

--- setup/config.js
+++ setup/config.js
@@ -1,13 +1,13 @@
 export const FILTER_FIELDS = {
   candidate_gender: {},
   state: {},
-  amt_raised_diff_pct_2018: {
-    num_buckets: 5,
+  vote_diff_2018: {
+    numBuckets: 5,
   },
 }
 
 export const BREAK_CHAR = ' to '
 
-export const SCATTERPLOT_X_FIELD = 'amt_raised_diff_2020'
+export const SCATTERPLOT_X_FIELD = 'amt_raised_diff_pct_2018'
 
 export const SCATTERPLOT_Y_FIELD = 'vote_diff_2018'
