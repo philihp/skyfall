d40ecb305796863899704b631baa08a03381082c
Kyle Schiller <kyleschiller@MacBook-Pro-2.local>
2020-09-23 16:52:35 -0700

    commit

--- setup/config.js
+++ setup/config.js
@@ -11,7 +11,6 @@ export const FILTER_FIELDS = {
 export const BREAK_CHAR = ' to '
 
 export const SCATTERPLOT_X_FIELD = 'amt_raised_diff_pct_2018'
-
 export const SCATTERPLOT_Y_FIELD = 'vote_diff_2018'
 
 export const CUSTOM_COLUMN_NAMES = {
