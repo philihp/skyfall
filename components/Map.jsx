489fdaf8a04f2cba4eef296373a32bd299062e41
Kyle Schiller <kyleschiller@gmail.com>
2020-09-23 04:10:31 -0700

    Make map hover temporary, click persistent (#35)
    * make map hover temporary, click persistent
    
    * add pointer cursor on clickable states
    
    * remove old styles
    
    Co-authored-by: Kyle Schiller <kyleschiller@MacBook-Pro-2.local>

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
 import states from '../data/states'
 
@@ -20,36 +20,64 @@ const nameToCodes = {
   Georgia: 'GA',
 }
 
+// MapChart supports temporary filters on hover, but reverts to the previous configuration onMouseLeave.
+// Clicking a state persists the new filter configuration.
 const MapChart = ({ filterOptions, setFilterOptions }) => {
+  const [previousFilterOptions, setPreviousFilterOptions] = useState({})
   const statesData = states()
 
-  const handleClick = (name) => {
+  const handleHover = (name) => {
     const code = nameToCodes[name]
+
     setFilterOptions({
       ...filterOptions,
       state: code,
     })
   }
 
+  const handleClick = (name) => {
+    const code = nameToCodes[name]
+
+    setPreviousFilterOptions({
+      ...filterOptions,
+      state: code,
+    })
+  }
+
   return (
-    <div style={{ width: 400 }}>
+    <div
+      style={{ width: 400 }}
+      onMouseEnter={() => {
+        setPreviousFilterOptions(filterOptions)
+      }}
+      onMouseLeave={() => {
+        setFilterOptions(previousFilterOptions)
+      }}
+    >
       <ComposableMap projection="geoAlbersUsa">
         <Geographies geography={statesData}>
           {({ geographies }) => (
             <>
               {geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  stroke="#FFF"
-                  geography={geo}
-                  fill="#6f44ff"
-                  opacity={statesSet.has(geo.properties.name) ? 1 : 0.15}
-                  onMouseOver={() => {
-                    if (statesSet.has(geo.properties.name)) {
-                      handleClick(geo.properties.name)
-                    }
-                  }}
-                />
+                <g cursor={statesSet.has(geo.properties.name) ? 'pointer' : ''}>
+                  <Geography
+                    key={geo.rsmKey}
+                    stroke="#FFF"
+                    geography={geo}
+                    fill="#6f44ff"
+                    opacity={statesSet.has(geo.properties.name) ? 1 : 0.15}
+                    onClick={() => {
+                      if (statesSet.has(geo.properties.name)) {
+                        handleClick(geo.properties.name)
+                      }
+                    }}
+                    onMouseOver={() => {
+                      if (statesSet.has(geo.properties.name)) {
+                        handleHover(geo.properties.name)
+                      }
+                    }}
+                  />
+                </g>
               ))}
             </>
           )}
