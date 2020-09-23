42ed789dc7f0b19868a2b641e29e89a45c04c6b8
Kyle Schiller <kyleschiller@MacBook-Pro-2.local>
2020-09-23 16:28:33 -0700

    replaceAll => replace

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
 export const dollarToFloat = (dollar) =>
-  Number.parseFloat(dollar.replaceAll(/\$|,/g, ''))
+  Number.parseFloat(dollar.replaceAll('$|,', ''))
 
 export const stringNumToFloat = (str) =>
   str === undefined ? undefined : Number.parseFloat(str.replace(',', ''))
