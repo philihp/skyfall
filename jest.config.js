66df568979d21a4c5d66734a10f5ae43e158896b
Philihp Busby <philihp@gmail.com>
2020-09-24 22:09:32 +0000

    fixes jest tests on mounted enzyme components

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setup-tests.js'],
   coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
   transform: {
-    '^.+\\.js$': 'babel-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
