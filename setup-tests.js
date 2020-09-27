71e82d3d981b47ad8d96e5bc7d63c632b41c6cc6
Philihp Busby <philihp@gmail.com>
2020-09-27 00:09:24 +0000

    setup tests for lunar

--- setup-tests.js
+++ setup-tests.js
@@ -1,5 +1,16 @@
+import React from 'react'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import lunar from '@airbnb/lunar'
+
+// LUNAR Setup
+
+React.useLayoutEffect = React.useEffect
+lunar.initialize({
+  defaultLocale: 'en',
+  defaultTimezone: 'UTC',
+  name: 'skyfall',
+})
 
 // Configure Enzyme with React 16 adapter
 Enzyme.configure({ adapter: new Adapter() })
