ef2a95df3c387663f035fdb33e8c66e6dfdee1a8
Philihp Busby <philihp@gmail.com>
2020-09-29 23:12:54 +0000

    add google analytics data (#52)

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Router from 'next/router'
+import * as gtag from '../lib/gtag'
 import '../styles/globals.css'
 import '../setup/setTheme'
 
 // eslint-disable-next-line react/prop-types
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
